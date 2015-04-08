13ac67fd436d27dac00fed7d1aa70622d25106d1
Frank Supper <frank.supper@gmail.com>
2015-04-08 10:36:07 +0200

    Wrapped angular in SI-Functions

--- Applications/Franky.Playground.WebApi/Scripts/app/app.js
+++ Applications/Franky.Playground.WebApi/Scripts/app/app.js
@@ -1,22 +1,24 @@
-'use strict';
+(function () {
+	'use strict';
 
-var mainApp = angular.module('mainApp', [
-	'ngAnimate',
-	'ui.router',
-	'itemControllers',
-	'itemServices'
-]);
+	var mainApp = angular.module('mainApp', [
+		'ngAnimate',
+		'ui.router',
+		'itemControllers',
+		'itemServices'
+	]);
 
-mainApp.run([
-	'$rootScope',
-	'$state',
-	'$stateParams',
-	function ($rootScope, $state, $stateParams) {
-		// It's very handy to add references to $state and $stateParams to the $rootScope
-		// so that you can access them from any scope within your applications.For example,
-		// <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
-		// to active whenever 'contacts.list' or one of its decendents is active.
-		$rootScope.$state = $state;
-		$rootScope.$stateParams = $stateParams;
-	}
-]);
+	mainApp.run([
+		'$rootScope',
+		'$state',
+		'$stateParams',
+		function ($rootScope, $state, $stateParams) {
+			// It's very handy to add references to $state and $stateParams to the $rootScope
+			// so that you can access them from any scope within your applications.For example,
+			// <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
+			// to active whenever 'contacts.list' or one of its decendents is active.
+			$rootScope.$state = $state;
+			$rootScope.$stateParams = $stateParams;
+		}
+	]);
+})();
--- Applications/Franky.Playground.WebApi/Scripts/app/routes.js
+++ Applications/Franky.Playground.WebApi/Scripts/app/routes.js
@@ -1,27 +1,28 @@
-﻿'use strict';
+﻿(function () {
+	'use strict';
 
-mainApp.config([
-	'$stateProvider',
-	'$urlRouterProvider',
-	'$locationProvider',
-	function ($stateProvider, $urlRouterProvider, $locationProvider) {
-		$urlRouterProvider.otherwise('/');
+	angular.module('mainApp').config([
+		'$stateProvider',
+		'$urlRouterProvider',
+		'$locationProvider',
+		function ($stateProvider, $urlRouterProvider, $locationProvider) {
+			$urlRouterProvider.otherwise('/');
 
-		$stateProvider
-			.state('dashboard', {
-				url: '/',
-				templateUrl: 'Template/Home/Dashboard',
-				data: { pageTitle: 'Dashboard' }
-			})
+			$stateProvider
+				.state('dashboard', {
+					url: '/',
+					templateUrl: 'Template/Home/Dashboard',
+					data: { pageTitle: 'Dashboard' }
+				})
 
-			.state('about', {
-				url: '/About',
-				templateUrl: 'Template/Home/About',
-				data: { pageTitle: 'About' }
-			});
+				.state('about', {
+					url: '/About',
+					templateUrl: 'Template/Home/About',
+					data: { pageTitle: 'About' }
+				});
 
-		// use the HTML5 History API
-		// currently kills the reload feature :-)
-		$locationProvider.html5Mode(true);
-	}
-]);
+			// use the HTML5 History API
+			$locationProvider.html5Mode(true);
+		}
+	]);
+})();
